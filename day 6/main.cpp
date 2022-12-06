05fb4c3e7b770a1e3e547b233b46a748cbdda762
Oscar Backström <oscar-backstrom@hotmail.com>
2022-12-06 15:26:11 +0100

    init day 6 and gitignore update

---
+++ day 6/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <regex>
+using namespace std;
+
+
+void part1(){
+    ifstream reader("input.in");
+    string buf;
+    getline(reader, buf);
+    cout << buf.length();
+    reader.close();
+}
+
+
+
+int main(int argc, char const *argv[]){
+
+
+    part1();
+    // part2();
+
+    return 0;
+}
