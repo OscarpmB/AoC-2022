7a5d8c45b6bf00ce6baaed6169a56779bad45226
Oscar Backström <oscar-backstrom@hotmail.com>
2022-12-15 17:48:26 +0100

    seems like long are to small

--- day 11/Main.java
+++ day 11/Main.java
@@ -1,45 +1,88 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.math.BigInteger;
 
 public class Main{
     public Main()throws Exception{
         String file = "test.txt";
         BufferedReader reader = new BufferedReader(new FileReader(file));
         String line;
-        Monkey monkies[] = new Monkey[7];
-        int counter[] = new int[7];
+        Monkey monkies[] = new Monkey[8];
+        int counter[] = new int[8];
         while(reader.ready()){
             line = reader.readLine();
             if(line.matches("Monkey\\s\\d:")){
                 int no = Integer.parseInt(line.substring(7, 8));
                 // Read the next 5 lines to create all monkies
                 String items[] = reader.readLine().replace(",", "").split(" ");
-                int op = Integer.parseInt(reader.readLine().split(" ")[7]);
+                // items = reader.readLine().split(" ");
+                String a[] = reader.readLine().split(" ");
+                String[] ops = a;
+                // items = reader.readLine().split(" ");
                 int dividor = Integer.parseInt(reader.readLine().split(" ")[5]);
+                // items = reader.readLine().split(" ");
                 int t = Integer.parseInt(reader.readLine().split(" ")[9]);
+                // items = reader.readLine().split(" ");
                 int f = Integer.parseInt(reader.readLine().split(" ")[9]);
-                monkies[no] = new Monkey(t, f, op, dividor);
+                // items = reader.readLine().split(" ");
+                monkies[no] = new Monkey(t, f, ops, dividor);
                 for(int i = 0; i < items.length ; i++){
                     if(items[i].matches("\\d+")){
-                        monkies[no].addItem(Integer.parseInt(items[i]));
+                        monkies[no].addItem(Long.parseLong(items[i]));
                     }
                 }
             }
         }
-
         /* Run 20 round and count how many times each monkey counts an item */
-        for(int i = 0; i < 20; i++){
-            for (int j = 7; j < monkies.length; j++) {
-                Integer item = monkies[j].items.pollFirst();
-                if(item == null) continue;
-                if(monkies[j].test(monkies[j].operation(item))){
-                    monkies[monkies[j].t-1].addItem(item);
-                }else{
-                    monkies[monkies[j].f-1].addItem(item);
+
+        int bound = monkies[4] == null ? 4 : 8;
+
+        for(int i = 0; i < 10_000; i++){
+            if(i==1 || i ==20 || (i % 1000) == 0){
+                System.out.println("After round " + i);
+                for (int j = 0; j < bound; j++) {
+                    System.out.println("Moneky" + j + " " + counter[j] );
                 }
             }
+            for (int j = 0; j < bound; j++) {
+                // System.out.println("j= " + j);
+                int inspections = 0;
+                for (Long item : monkies[j].items) {
+                    if(item == null) continue;
+                    long modified = monkies[j].operation(item);
+                    if(monkies[j].test(modified)){
+                        monkies[monkies[j].t].addItem(modified);
+                    }else{
+                        monkies[monkies[j].f].addItem(modified);
+                    }
+                    counter[j]++;
+                    inspections++;
+                }
+                for(int k = 0; k < inspections; k++){
+                    monkies[j].items.pop();
+                }
+                
+            }
         }
+        // for (int i : counter) {
+        //     System.out.println(i);
+        // }
 
+        // find the two larges values
+        int max1 = 0;
+        int max2 = 0;
+        for(int c : counter){
+            if (c > max1){
+                max2= max1;
+                max1 = c;
+            }else if(max1 > c && c > max2){
+                max2 = c;
+            }
+        }
+        BigInteger A = BigInteger.valueOf(max1);
+        BigInteger B = BigInteger.valueOf(max2);
+        BigInteger part1 = A.multiply(B);
+        System.out.println("part1 = " + part1);
         reader.close();
     }
 
--- day 11/Monkey.java
+++ day 11/Monkey.java
@@ -1,31 +1,50 @@
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Monkey {
-    private int divisor;
-    public LinkedList<Integer> items;
+
+    private long divisor;
+    public LinkedList<Long> items;
     public int t;
     public int f;
-    private int operator;
+    private String operator;
+    private String operand1;
+    private String operand2;
 
 
-    public Monkey(int tr, int fa, int op, int d) {
+    public Monkey(int tr, int fa, String[] op, long d) {
         this.divisor = d;
         this.items = new LinkedList<>();
         this.f = fa;
         this.t = tr;
-        this.operator = op;
+        this.operator = op[op.length-2];
+        this.operand1 = op[op.length-3];
+        this.operand2 = op[op.length-1];
     }
 
-    public int operation(int i){
-        return this.operator * i;
+    public long operation(long i){
+
+        long old;
+        if(this.operand2.equals("old")){
+            old = i;
+        }else{
+            old = Long.parseLong(this.operand2);
+        }
+        switch(this.operator){
+            case "+":
+                return i + (old);
+            case "*":
+                return i*old;
+        }
+        return 0;
     }
 
-    public void addItem(int i){
+    public void addItem(long i){
         this.items.add(i);
     }
 
-    public boolean test(int a){
+    public boolean test(long a){
         return (a % this.divisor) == 0 ? true : false;
     }
 }
